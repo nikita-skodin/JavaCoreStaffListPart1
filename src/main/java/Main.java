92348b7be003139452315d4325f0304781a389c2
Nikita_Skodin <skodin36@gmail.com>
2024-01-05 15:14:03 +0300

    gitignore has been updated log4j2 has been added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class Main {
 
     // TODO think about command line arguments
     public static void main(String[] args) {
-
+        log.info("Application is running");
     }
 
 }
